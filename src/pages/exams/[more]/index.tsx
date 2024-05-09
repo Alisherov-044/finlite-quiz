8a91a942fa3531d1fe8433301ff0bcb77eae0978
Alisherov-044 <alisherov044@gmail.com>
2024-05-09 19:11:49 +0500

    fix: removed exam title

--- src/pages/exams/[more]/index.tsx
+++ src/pages/exams/[more]/index.tsx
@@ -1,13 +1,8 @@
 import { Loading, QuizResult } from "@/components";
-import { TExam } from "@/components/cards/exam-card";
 import { TUser } from "@/components/cards/user-card";
 import { useSelector, useTranslate } from "@/hooks";
 import { axiosPrivate } from "@/lib";
-import {
-    EXAMS_EDIT_URL,
-    EXAM_RESULT_URL,
-    STUDENTS_EDIT_URL,
-} from "@/utils/urls";
+import { EXAM_RESULT_URL, STUDENTS_EDIT_URL } from "@/utils/urls";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { TExamResult } from "../[slug]";
@@ -46,27 +41,13 @@ export default function ExamUserResultPage() {
         }
     );
 
-    const { data: exam, isLoading: isExamLoading } = useQuery<TExam>("exam", {
-        queryFn: async () =>
-            await axiosPrivate
-                .get(EXAMS_EDIT_URL(Number(id)), {
-                    headers: {
-                        Authorization: `Bearer ${access_token}`,
-                    },
-                })
-                .then((res) => res.data.data),
-    });
-
-    if (isUserLoading || isExamLoading || isResultLoading) return <Loading />;
+    if (isUserLoading || isResultLoading) return <Loading />;
 
     return (
         <main className="my-3">
             <div className="container">
                 <Flex className="items-center justify-between mb-4">
                     <Flex className="flex-col">
-                        <Typography className="!text-blue-900 !text-xl">
-                            {t(`Bo'lim: ${exam?.title}`)}
-                        </Typography>
                         <Typography className="!text-blue-900 !text-xl">
                             {t(
                                 `O'quvchi: ${
